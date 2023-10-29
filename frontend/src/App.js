a3adf89a00e3b59d467c6183a16d69837cda9886
Ankush Madharha <ankushmadharha@gmail.com>
2023-10-29 19:25:00 -0400

    added Featured component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Nav from './components/Nav.js';
 import Image from './components/Image.js';
 import Discover from './components/Discover';
+import Featured from './components/Featured';
 
 function App() {
   return (
     <div>
       <Nav />
       <Image />
-      <Discover/>
+      <Discover />
+      <Featured />
     </div>
   );
 }
