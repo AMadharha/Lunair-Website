035f3339d4125dec7672bf25b9087e4197dd3d1f
Pranith Peesapati <pranithvrp@gmail.com>
2023-11-03 00:45:24 -0400

    Added interactivity to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './assets/css/global.css'
 import styles from './assets/css/Footer.module.css'
 import instagram from './assets/images/instagram.png'
 import tiktok from './assets/images/tiktok.png'
+import instagramHover from './assets/images/instagram-hover.png'
+import tiktokHover from './assets/images/tiktok-hover.png'
 
 const Footer = () => {
+    const [isInstagramHovered, setInstagramHovered] = useState(false);
+    const [isTikTokHovered, setTikTokHovered] = useState(false);
+
     return (
         <div className={styles.container}>
             <div className={styles.footerContent}>
                 <div className={styles.quickLinks}>
                     <h1 className={styles.header}>Quick Links</h1>
-                    <a className={styles.link}>Back to Top</a>
+                    <a className={styles.link} href="#">Back to Top</a>
                     <a className={styles.link}>Shop</a>
                     <a className={styles.link}>Contact Us</a>
                 </div>
                 <div className={styles.socialMedia}>
-                    <img src={instagram} alt="Instagram Logo" className={styles.socialIcon} />
-                    <img src={tiktok} alt="tikTok Logo" className={styles.socialIcon} />
+                    <a href="https://www.instagram.com/lunair.co/" target="_blank">
+                        <img
+                            src={isInstagramHovered ? instagramHover : instagram}
+                            alt="Instagram Logo"
+                            className={styles.socialIcon}
+                            onMouseEnter={() => setInstagramHovered(true)}
+                            onMouseLeave={() => setInstagramHovered(false)}
+                        />
+                    </a>
+                    <img
+                        src={isTikTokHovered ? tiktokHover : tiktok}
+                        alt="TikTok Logo"
+                        className={styles.socialIcon}
+                        onMouseEnter={() => setTikTokHovered(true)}
+                        onMouseLeave={() => setTikTokHovered(false)}
+                    />
                 </div>
             </div>
         </div>
