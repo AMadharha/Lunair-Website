54f6f62d0633abb4f7483adbbf768ea1905609d2
Ankush Madharha <ankushmadharha@gmail.com>
2024-01-02 15:42:54 -0500

    fixed tab sizing

--- frontend/src/components/Data/Data.js
+++ frontend/src/components/Data/Data.js
@@ -3,26 +3,26 @@ import leviHoodie from '../assets/images/levi-white.png'
 import sasukeHoodie from '../assets/images/sasuke-black.png'
 
 const data = {
-  productItems: [
-    {
-      id: "1",
-      name: "Sasuke",
-      price: 60,
-      image: sasukeHoodie,
-    },
-    {
-      id: "2",
-      name: "Gojo",
-      price: 60,
-      image: gojoHoodie,
-    },
-    {
-      id: "3",
-      name: "Levi",
-      price: 60,
-      image: leviHoodie,
-    },
-  ],
+    productItems: [
+        {
+            id: "1",
+            name: "Sasuke",
+            price: 60,
+            image: sasukeHoodie,
+        },
+        {
+            id: "2",
+            name: "Gojo",
+            price: 60,
+            image: gojoHoodie,
+        },
+        {
+            id: "3",
+            name: "Levi",
+            price: 60,  
+            image: leviHoodie,
+        },
+    ],
 };
 
 
